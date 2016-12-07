45a15df6f5b09008078623cc70b0d99700edb973
Tony Gao <tony.gao000@gmail.com>
2016-12-08 02:07:02 +0800

    add persist storage helper

---
+++ rn_common/PersistStorage.js
@@ -0,0 +1,25 @@
+/**
+ * Persist storage helper.
+ */
+
+export function save(key, value, callback) {
+    try {
+        await AsyncStorage.setItem(key, value);
+        if (callback !== undefined) {
+            callback();
+        }
+    } catch (error) {
+        // Error saving data
+    }
+}
+
+export async function retrieve(key, callback) {
+    try {
+        const value = await AsyncStorage.getItem(key);
+        if (callback !== undefined) {
+            callback(value);
+        }
+    } catch (error) {
+        // error
+    }
+}
